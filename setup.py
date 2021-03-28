3d18ae3f2a5a015bfe63a143abafb6a528c8422a
Eric Henry <eric.henry2172@gmail.com>
2021-03-28 17:12:11 -0400

    Bump version

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ import setuptools
 
 setuptools.setup(
     name="xgbatch",
-    version="0.0.1",
+    version="0.0.2",
     author="Eric Henry",
     description="High Performance Serving for XGBoost",
     url="https://github.com/ehenry2/xgbatch",
